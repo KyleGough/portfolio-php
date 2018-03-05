b735182da4052633ead18be47c9cd3361e5dbaf1
Kyle Gough <kylegough98@gmail.com>
2018-03-05 23:54:09 +0000

    Remove Materialize JS replace with CDN

--- head.php
+++ head.php
@@ -1,12 +1,9 @@
 <meta charset="UTF-8">
-<link href="https://fonts.googleapis.com/css?family=Material+Icons|Cinzel|News+Cycle" rel="stylesheet"> <!-- Import Google Fonts-->
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
-
-<!--<link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection">--> <!--Import materialize.css-->
+<link href="https://fonts.googleapis.com/css?family=Material+Icons|Cinzel|News+Cycle" rel="stylesheet"> <!--Import Google Fonts-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"> <!--Materialize CSS-->
 <link type="text/css" rel="stylesheet" href="css/style.css" media="screen,projection"> <!--Import custom CSS-->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<script type="text/javascript" src="js/materialize.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script> <!--,Materialize JS-->
 <script type="text/javascript" src="js/main.js"></script>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="icon" href="./favicon.ico" type="image/x-icon">
